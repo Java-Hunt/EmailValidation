599641cc2dc21c7dbda94e44f575881c3fc85fb1
Rahul-trainingPortal <rk980244@gmail.com>
2025-06-08 13:26:32 +0530

    commit code

---
+++ src/main/java/com/nic/emailnotification/controller/EmailController.java
@@ -0,0 +1,8 @@
+package com.nic.emailnotification.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+}
---
+++ src/main/java/com/nic/emailnotification/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.nic.emailnotification.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+	
+	 @Autowired
+	    private JavaMailSender mailSender;
+
+	    @Autowired
+	    private TemplateEngine templateEngine;
+
+	    public void sendWelcomeEmail(String to, String name) {
+	        Context context = new Context();
+	        context.setVariable("name", name);
+
+	        String processHtml = templateEngine.process("welcome-email", context);
+
+	        MimeMessage mimeMessage = mailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+	        try {
+	            helper.setText(processHtml, true); // true indicates HTML
+	            helper.setTo(to);
+	            helper.setSubject("Welcome to Our Service!");
+	            helper.setFrom("kumarrahul21269@gmail.com");
+	            mailSender.send(mimeMessage);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+}
